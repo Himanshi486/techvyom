fb922f40c6bb8ef0f113fcc5906b02da22b26292
Himanshi <himanshisehrawat568@gmail.com>
2025-12-07 22:02:06 +0530

    Force add connect.php

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$conn = mysqli_connect(
+    getenv("DB_HOST"),
+    getenv("DB_USER"),
+    getenv("DB_PASS"),
+    getenv("DB_NAME"),
+    getenv("DB_PORT")
+);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+?>
+
