73042472cae3a86ae317c17249a3d98a0d76c899
Himanshi486 <himanshisehrawat568@gmail.com>
2025-12-16 20:34:16 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,16 @@
 <?php
+$credDir = __DIR__ . '/credentials';
+$credFile = $credDir . '/alumni-service.json';
+
+if (!file_exists($credFile)) {
+    $googleCreds = getenv('GOOGLE_CREDENTIALS');
+    if ($googleCreds) {
+        if (!is_dir($credDir)) {
+            mkdir($credDir, 0755, true);
+        }
+        file_put_contents($credFile, $googleCreds);
+    }
+}
 $host = getenv("DB_HOST");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
